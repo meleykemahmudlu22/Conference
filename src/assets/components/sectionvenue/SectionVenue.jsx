f3ab163a09e81abcb2d80c6c26abed6f010e1aa7
F3rid.rrr <seid2008ferid@gmail.com>
2026-07-18 23:50:26 +0400

    fix: redesign venue page and map button link to google maps

--- src/assets/components/sectionvenue/SectionVenue.jsx
+++ src/assets/components/sectionvenue/SectionVenue.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaUniversity } from "react-icons/fa";
-import { FaMap } from "react-icons/fa";
+import { FaMap, FaMapMarkerAlt } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 import "./venue.css"
 
@@ -25,11 +25,18 @@ function SectionVenue (){
                  <h2>Mingachevir State University</h2>
               </div>
               <div className="locationmap">
-               < FaLocationDot/>
+               < FaMapMarkerAlt className="location-marker-icon" />
                <p>120 Hasan Aliyev str., Khirdalan city AZ0101, Azerbaijan</p>
               </div>
               <div className="locationbtn">
-                <button>< FaMap/>Wiew on Map</button>
+                <a 
+                  href="https://www.google.com/maps?q=Mingachevir+State+University" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="map-link-wrapper"
+                >
+                  <button>< FaMap/>View on Map</button>
+                </a>
                
               </div>
              
